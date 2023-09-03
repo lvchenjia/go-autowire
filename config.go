b1e127dd70fbed22ae0513c34638172aa74176bd
Xun Qin <lvchenjia@163.com>
2023-09-03 22:51:17 +0800

    Update

--- config.go
+++ config.go
@@ -1,8 +1,19 @@
 package main
 
-//SQL Config
-const config_driver_name = "mysql"
-const config_data_source_name = "uname:pwd/test?charset=utf8"
+import "flag"
 
-//Dev Config
-const config_debug = true
+// SQL Config
+var config_driver_name = "mysql"
+var config_data_source_name = "root:root@/test?charset=utf8"
+
+// Dev Config
+var config_debug = true
+
+func getConfig() {
+	flag.StringVar(&config_driver_name, "driver", "mysql", "Database driver name")
+	flag.StringVar(&config_data_source_name, "dsn", "root:root@/test?charset=utf8", "Data source name")
+	flag.BoolVar(&config_debug, "debug", true, "Enable debugging")
+
+	// 解析命令行参数
+	flag.Parse()
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 func main() {
+	getConfig()
+
 	if config_debug {
 		printlnYellow("Debug Mode")
 		test()
